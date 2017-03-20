3891d16d1802b374bf7c72c719b8210b771a9123
d61h6k4 <dbihbka@protonmail.ch>
2017-03-20 23:24:58 +0300

    Add test case when we nothin filter stay everything same'

--- test/NLP/Corpora/TestDictionary.hs
+++ test/NLP/Corpora/TestDictionary.hs
@@ -7,20 +7,37 @@ import NLP.Corpora.Dictionary
 
 
 dictionarySpec =
-    describe "Add document to dict and doc2bow" $ do
-      it "return list of bag of words" $ do
-        let document =
-              [ "Hooks"
-              , "support"
-              , "passing"
-              , "values"
-              , "to"
-              , "spec"
-              , "items"
-              , "to"
-              ]
-        dict <- new
-        dict' <- addDocument document dict
-        document_bow <- doc2bow document dict'
-        length document_bow `shouldBe` length document - 1
-        sum (map snd document_bow) `shouldBe` length document
+  describe "Add document to dict and doc2bow" $ do
+    it "return list of bag of words" $ do
+      let document =
+            [ "Hooks"
+            , "support"
+            , "passing"
+            , "values"
+            , "to"
+            , "spec"
+            , "items"
+            , "to"
+            ]
+      dict <- new
+      dict' <- addDocument document dict
+      document_bow <- doc2bow document dict'
+      length document_bow `shouldBe` length document - 1
+      sum (map snd document_bow) `shouldBe` length document
+    it "filterExtremes with unfliterable params stay all without changes" $ do
+      let document =
+            [ "Hooks"
+            , "support"
+            , "passing"
+            , "values"
+            , "to"
+            , "spec"
+            , "items"
+            , "to"
+            ]
+      dict <- new
+      dict' <- addDocument document dict
+      dict'' <- filterExtremes 0 3 (length document) dict'
+      document_bow' <- doc2bow document dict'
+      document_bow'' <- doc2bow document dict''
+      document_bow'' `shouldBe` document_bow'
