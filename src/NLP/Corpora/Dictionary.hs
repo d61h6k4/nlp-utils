e2471946fac98eb347cdef70ec75b325f807f334
d61h6k4 <dbihbka@protonmail.ch>
2017-03-20 23:24:32 +0300

    Add filter extremes functionality

--- src/NLP/Corpora/Dictionary.hs
+++ src/NLP/Corpora/Dictionary.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE LambdaCase #-}
 {-|
@@ -5,6 +6,8 @@ This module implements the concept of Dictionary from (gensim <https://radimrehu
 -}
 module NLP.Corpora.Dictionary where
 
+import Data.List (sortBy)
+import Data.Function (on)
 import Data.Foldable (toList)
 import Data.Maybe (isJust, fromJust)
 import Control.Monad (foldM, mapM)
@@ -12,12 +15,18 @@ import Data.Text (Text)
 
 import qualified Data.HashTable.IO as HashTable
 import qualified Data.Map.Strict as Map
+import qualified Data.Set as Set
 
 
+-- | Main structure containing
+-- `token2id` hash table where collect pair of token and tokenid
+-- `id2token` hash table with tokenid and token pairs (use for restore token by tokenid)
+-- `dfs` is document frequencies (use for filter too frequent tokens)
 data Dictionary = Dictionary
   { token2id :: HashTable.CuckooHashTable Text Int
   , id2token :: HashTable.CuckooHashTable Int Text
   , currentTokenId :: Int
+  , dfs :: HashTable.CuckooHashTable Int Int
   }
 
 
@@ -26,7 +35,10 @@ new :: IO Dictionary
 new = do
   t2id <- HashTable.new
   id2t <- HashTable.new
-  return $ Dictionary {token2id = t2id, id2token = id2t, currentTokenId = 0}
+  dfs_ <- HashTable.new
+  return $
+    Dictionary
+    {token2id = t2id, id2token = id2t, currentTokenId = 0, dfs = dfs_}
 
 -- | Add document to dictionary. Insert all new unique tokens
 -- from document to dictionary. Document is the foldable (e.g. list)
@@ -42,8 +54,12 @@ addDocument text dict =
            let currentTokenId' = currentTokenId dict' + 1
            in HashTable.insert (token2id dict') token currentTokenId' >>
               HashTable.insert (id2token dict') currentTokenId' token >>
+              HashTable.insert (dfs dict') currentTokenId' 1 >>
               return (dict' {currentTokenId = currentTokenId'})
-         Just _ -> return dict')
+         Just currentTokenId' ->
+           HashTable.lookup (dfs dict') currentTokenId' >>= \value ->
+             HashTable.insert (dfs dict') currentTokenId' (maybe 1 (+ 1) value) >>
+             return dict')
     dict
     text
 
@@ -73,3 +89,42 @@ doc2bow text dict =
 -- with given id return Nothing.
 get :: Int -> Dictionary -> IO (Maybe Text)
 get tokenId dict = HashTable.lookup (id2token dict) tokenId
+
+
+-- | Filter out tokens that appear in
+--
+-- 1. less than `no_below` documents (absolute number) or
+-- 2. more than `no_above` documents (absolute number, because we can't count
+--    how many documents stay after filter extremes).
+-- 3. after (1) and (2), keep only the first `keep_n` most frequent tokens (or keep all if `None`).
+--
+-- After the pruning, shrink resulting gaps in word ids.
+--
+-- **Note**: Due to the gap shrinking, the same word may have a different
+-- word id before and after the call to this function!
+filterExtremes :: Int -> Int -> Int -> Dictionary -> IO Dictionary
+filterExtremes no_below no_above keep_n dict =
+  HashTable.toList (dfs dict) >>=
+  return . filter (\(k, v) -> v > no_below && v < no_above) >>=
+  return . take keep_n . sortBy (compare `on` snd) >>=
+  flip filterOthers dict
+
+filterOthers :: [(Int, Int)] -> Dictionary -> IO Dictionary
+filterOthers notOthers Dictionary {..} = do
+  let notOthersIds = Set.fromList (map fst notOthers)
+  token2id' <-
+    HashTable.toList token2id >>=
+    return . filter (\(k, v) -> Set.member v notOthersIds) >>=
+    HashTable.fromList
+  id2token' <-
+    HashTable.toList id2token >>=
+    return . filter (\(k, v) -> Set.member k notOthersIds) >>=
+    HashTable.fromList
+  dfs' <- HashTable.fromList notOthers
+  return
+    (Dictionary
+     { token2id = token2id'
+     , id2token = id2token'
+     , dfs = dfs'
+     , currentTokenId = currentTokenId
+     })
